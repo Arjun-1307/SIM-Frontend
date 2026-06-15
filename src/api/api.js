9c3a28c4896a39b0cf5799d0aedc5d60a1c881c5
Arjun-1307 <arjunguduru2007@gmail.com>
2026-06-15 17:39:52 +0530

    Update production API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5002",
+  baseURL: "https://sim-springboot.onrender.com/api/auth/login",
 });
 
 API.interceptors.request.use(
